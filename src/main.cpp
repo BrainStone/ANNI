1aa8bd03506432f2e8d9a7374d4a5b11d2f9a1d2
BrainStone <yannick@tedworld.de>
2017-12-17 13:06:06 +0100

    Extended testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+#if defined(__DEBUG__)
+	#define BUILD_TYPE "debug"
+#elif defined(__NDEBUG__)
+	#define BUILD_TYPE "release"
+#else
+	#define BUILD_TYPE "unknow"
+#endif
+
 int main() {
 	std::cout << "Hello World!" << std::endl;
+	std::cout << "This application is compiled as a " << sizeof(size_t) * 8 << " bit binary." << std::endl;
+	std::cout << "This is a " BUILD_TYPE " build." << std::endl;
 }
