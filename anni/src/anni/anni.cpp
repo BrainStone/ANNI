e2657f6e857d526359207834ff9f625ea20d61ad
BrainStone <yannick@tedworld.de>
2018-06-21 19:27:52 +0200

    Apply license headers

--- anni/src/anni/anni.cpp
+++ anni/src/anni/anni.cpp
@@ -1,1 +1,5 @@
+//  
+// Copyright (c) 2018 Yannick Schinko. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+//  
 #include "anni.hpp"
