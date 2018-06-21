2345d2b3c91401618f65e492ca16278c90be8686
BrainStone <yannick@tedworld.de>
2018-06-21 21:29:53 +0200

    Added MinMaxTree base file

---
+++ anni/src/anni/min_max_tree.hpp
@@ -0,0 +1,11 @@
+//  
+// Copyright (c) 2018 Yannick Schinko. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+//  
+#pragma once
+
+namespace anni {
+	class MinMaxTree {
+
+	};
+}
